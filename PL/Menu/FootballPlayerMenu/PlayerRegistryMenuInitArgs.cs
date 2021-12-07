63440eae3696d5263dfaf3b3716705a75fe42fa8
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-12-07 22:37:16 +0300

    fix

---
+++ PL/Menu/FootballPlayerMenu/PlayerRegistryMenuInitArgs.cs
@@ -0,0 +1,11 @@
+﻿using BLL;
+
+namespace PL
+{
+    public class PlayerRegistryMenuInitArgs : MenuInitArgs
+    {
+        public readonly IFootBallService service;
+
+        public PlayerRegistryMenuInitArgs(IFootBallService service) => this.service = service;
+    }
+}
